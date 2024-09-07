082dbf945f9c02af288e584eb95f0f726b53091b
YangxGeon <https://github.com/YangxGeon>
2024-09-07 18:29:58 +0900

    24.09.07

---
+++ 2024.09/2024.09.07/PCCE2.py
@@ -0,0 +1,8 @@
+# [PCCE 기출문제] 2번 / 피타고라스의 정리
+#  https://school.programmers.co.kr/learn/courses/30/lessons/250132
+
+a = int(input())
+c = int(input())
+
+b_square = c*c - a*a
+print(b_square)
---
+++ 2024.09/2024.09.07/PCCE3.py
@@ -0,0 +1,12 @@
+# [PCCE 기출문제] 3번 / 나이 계산
+# https://school.programmers.co.kr/learn/courses/30/lessons/250131
+
+year = int(input())
+age_type = input()
+
+if age_type == "Korea":
+    answer = 2030 - year + 1
+elif age_type == "Year":
+    answer = 2030 - year
+
+print(answer)
