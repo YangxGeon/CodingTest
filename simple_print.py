4b793f0a0becff99c1b19e07565f16c1bcdde99f
YangxGeon <https://github.com/YangxGeon>
2024-09-07 15:37:39 +0900

    [setting]: 파이썬 프로젝트 생성

---
+++ simple_print.py
@@ -0,0 +1,11 @@
+print("Hello World")
+
+# 파이썬에서 f 문자열은 **포매팅 문자열(f-string)
+# 문자열 안에 변수를 삽입하거나 표현식을 평가하여 문자열에 포함시키는 방법
+a=5
+b=3
+print(f"{a}+{b}={a+b}")
+
+name = "yangxgeon"
+age = 24
+print(f"My name is {name} and I am {age} years old")
